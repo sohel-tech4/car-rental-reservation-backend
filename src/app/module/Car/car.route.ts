955862d554cc9a3ab8acc861c6cd27d114a1fe8f
Sohel Rana <soheltech4@gmail.com>
2024-06-21 19:43:21 +0600

    add car

---
+++ src/app/module/Car/car.route.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import validateRequest from '../../middleware/validateRequest';
+import { CarValidation } from './car.validation';
+import { CarControllers } from './car.controller';
+import Auth from '../../middleware/auth';
+import { USER_ROLE } from '../User/user.constant';
+
+const router = express.Router();
+
+router.put('/return', Auth(USER_ROLE.admin), CarControllers.returnCar);
+router.post(
+  '/',
+  Auth(USER_ROLE.admin),
+  validateRequest(CarValidation.carSchemaValidation),
+  CarControllers.createCar,
+);
+router.get('/', CarControllers.getAllCars);
+router.get('/:id', CarControllers.getSingleCar);
+router.put(
+  '/:id',
+  validateRequest(CarValidation.updateCarSchema),
+  CarControllers.updateCar,
+);
+router.delete('/:id', CarControllers.deleteCar);
+
+export const CarRoutes = router;
